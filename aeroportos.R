f2cc98e8f4cf54f2d2e98171a3c2c5878d04d79d
lfpdroubi <lfpdroubi@gmail.com>
2020-04-20 17:01:45 -0300

    minor modifications.

--- aeroportos.R
+++ aeroportos.R
@@ -7,11 +7,15 @@ sbfl <- st_read("aeroportos/SBFL.geojson")
 sbfl <- st_sf(st_drop_geometry(sbnf), 
               geometry = st_combine(st_geometry(sbfl)))
 
-sbfl$nup <- NULL
-sbfl$protocolo <- NULL
-sbfl$ref <- NULL
+sbfl$nup <- NA
+sbfl$protocolo <- NA
+sbfl$ref <- NA
 
 sbfl$perimetro <- st_length(st_geometry(sbfl))
 sbfl$area <- st_area(sbfl)
 
 sbfl$municipio <- 8105
+
+aeroportos <- rbind(sbnf, sbfl)
+
+st_write(aeroportos, "aeroportos.geojson", delete_dsn = TRUE)
