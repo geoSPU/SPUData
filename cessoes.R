91e5ef4e1a00dc35067bdb761ba4f62ae6930799
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-07-31 14:59:32 -0300

    Adds Beira Mar Continental.

--- cessoes.R
+++ cessoes.R
@@ -2,32 +2,32 @@ library(sf)
 
 cessoes <- read_sf("cessoes.geojson")
 
-beira_mar_sj <- st_zm(st_read("cessoes/8327000585003 - Beira-Mar de São José.kml"))
+beira_mar_continental <- st_zm(st_read("cessoes/8105006055002 - Cessão Beira Mar continental - Estreito.kml"))
 
 attr <- data.frame(
   destinacao = "cessao",
   tipo = "terrestre",
-  rip = "8327 00058.500-3",
-  interessado = "Prefeitura Municipal de São José",
-  area = st_area(beira_mar_sj),
-  area_uniao = st_area(beira_mar_sj),
+  rip = "8105 00605.500-2",
+  interessado = "Prefeitura Municipal de Florianópolis",
+  area = st_area(beira_mar_continental),
+  area_uniao = st_area(beira_mar_continental),
   inicio = NA,
   prazo = NA,
   vigencia = NA,
-  cnpj = "81.140.303/0001-01",
-  nup = "11452.000729/99-82",
+  cnpj = "82.892.282/0001-43",
+  nup = "04972.001214/2004-07",
   protocolo = NA,
-  ref = NA,
+  ref = 9087623,
   concedida = TRUE,
-  municipio = 8327,
+  municipio = 8105,
   logradouro = NA,
   aval = NA,
   dataaval = NA,
   refaval = NA,
   onerosa = FALSE)
 
-beira_mar_sj <- st_sf(attr, geometry = st_geometry(beira_mar_sj))
+beira_mar_continental <- st_sf(attr, geometry = st_geometry(beira_mar_continental))
 
-cessoes <- rbind(cessoes, beira_mar_sj)
+cessoes <- rbind(cessoes, beira_mar_continental)
 
 st_write(cessoes, "cessoes.geojson", delete_dsn = TRUE)
