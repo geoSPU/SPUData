fd9ddf3380790dba37bf129131e3cdfcf54ed9a2
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-07-31 15:32:52 -0300

    Adds Parque de Coqueiros.

--- cessoes.R
+++ cessoes.R
@@ -2,32 +2,32 @@ library(sf)
 
 cessoes <- read_sf("cessoes.geojson")
 
-terminalRitaMaria <- st_zm(st_read("cessoes/8105006785000 - Terminal Rita Maria.kml"))
+parqueCoqueiros <- st_zm(st_read("cessoes/8105001735005 - Parque de Coqueiros.kml"))
 
 attr <- data.frame(
   destinacao = "cessao",
   tipo = "terrestre",
-  rip = "8105 00678.500-0",
-  interessado = "Governo do Estado de Santa Catarina",
-  area = st_area(terminalRitaMaria),
-  area_uniao = st_area(terminalRitaMaria),
-  inicio = as.Date("2020-07-31"),
-  prazo = 20,
-  vigencia = as.Date("2040-07-31"),
-  cnpj = "82.951.229/0001-76",
-  nup = "04972.002534/2008-08",
+  rip = "8105 00173.500-5",
+  interessado = "Prefeitura Municipal de Florianopolis",
+  area = st_area(parqueCoqueiros),
+  area_uniao = st_area(parqueCoqueiros),
+  inicio = NA,
+  prazo = NA,
+  vigencia = NA,
+  cnpj = "82.892.282/0001-43",
+  nup = "11452.001000/0081",
   protocolo = NA,
   ref = NA,
   concedida = TRUE,
   municipio = 8105,
   logradouro = NA,
-  aval = 24000000,
-  dataaval = as.Date("2020-02-06"),
-  refaval = 6358411,
-  onerosa = TRUE)
+  aval = NA,
+  dataaval = NA,
+  refaval = NA,
+  onerosa = FALSE)
 
-beira_mar_continental <- st_sf(attr, geometry = st_geometry(beira_mar_continental))
+parqueCoqueiros <- st_sf(attr, geometry = st_geometry(parqueCoqueiros))
 
-cessoes <- rbind(cessoes, beira_mar_continental)
+cessoes <- rbind(cessoes, parqueCoqueiros)
 
 st_write(cessoes, "cessoes.geojson", delete_dsn = TRUE)
