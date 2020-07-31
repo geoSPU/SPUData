32174afd6cc1293ac2e1b9a0b8569764ab20a082
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-07-31 15:20:47 -0300

    Adds Terminal Rita Maria.

--- cessoes.R
+++ cessoes.R
@@ -2,29 +2,29 @@ library(sf)
 
 cessoes <- read_sf("cessoes.geojson")
 
-beira_mar_continental <- st_zm(st_read("cessoes/8105006055002 - Cessão Beira Mar continental - Estreito.kml"))
+terminalRitaMaria <- st_zm(st_read("cessoes/8105006785000 - Terminal Rita Maria.kml"))
 
 attr <- data.frame(
   destinacao = "cessao",
   tipo = "terrestre",
-  rip = "8105 00605.500-2",
-  interessado = "Prefeitura Municipal de Florianópolis",
-  area = st_area(beira_mar_continental),
-  area_uniao = st_area(beira_mar_continental),
-  inicio = NA,
-  prazo = NA,
-  vigencia = NA,
-  cnpj = "82.892.282/0001-43",
-  nup = "04972.001214/2004-07",
+  rip = "8105 00678.500-0",
+  interessado = "Governo do Estado de Santa Catarina",
+  area = st_area(terminalRitaMaria),
+  area_uniao = st_area(terminalRitaMaria),
+  inicio = as.Date("2020-07-31"),
+  prazo = 20,
+  vigencia = as.Date("2040-07-31"),
+  cnpj = "82.951.229/0001-76",
+  nup = "04972.002534/2008-08",
   protocolo = NA,
-  ref = 9087623,
+  ref = NA,
   concedida = TRUE,
   municipio = 8105,
   logradouro = NA,
-  aval = NA,
-  dataaval = NA,
-  refaval = NA,
-  onerosa = FALSE)
+  aval = 24000000,
+  dataaval = as.Date("2020-02-06"),
+  refaval = 6358411,
+  onerosa = TRUE)
 
 beira_mar_continental <- st_sf(attr, geometry = st_geometry(beira_mar_continental))
 
