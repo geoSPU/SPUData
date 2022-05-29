8fd674e43752fcce06e48cb5485a9c261d1070a8
Luiz F P Droubi <lfpdroubi@gmail.com>
2022-05-29 17:43:31 -0300

    adds 04972.003379/2019-91 point.

--- cessoes.R
+++ cessoes.R
@@ -1,9 +1,16 @@
 library(sf)
 
-cessoes <- read_sf("cessoes.geojson")
+cessoes <- read_sf("cessoes.geojson") 
 
-cessaoNova <- read_sf("cessoes/10154-137136-2019-92.geojson")
+cessao_nova <- read_sf("cessoes/04972-005004-2018-85.geojson")
 
-cessoes <- rbind(cessoes, cessaoNova)
+# cessao_nova <- read_sf("G:/.shortcut-targets-by-id/1nUTlixFUMjdbt5j123GxmYTojiaoNMMS/SPUVIZ_Projeto EAN Portos/polígonos e tabelas já feitas/GEOJSON_criados/04972.000936_2005-17.geojson")
+
+cessao_nova$cnpj <- NULL
+
+# cessaoNova <- read_sf("cessoes/10154-146304-2021-55_terra.geojson")
+# cessaoNova2 <- read_sf("cessoes/10154-146304-2021-55_agua.geojson")
+# 
+cessoes <- rbind(cessoes, cessao_nova)
 
 st_write(cessoes, "cessoes.geojson", delete_dsn = TRUE)
